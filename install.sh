6bd066832377058b8ec07ffb6244b75dd26146d6
ellie timoney <flrl@users.noreply.github.com>
2021-03-06 13:42:23 +1100

    add install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+DF="/c/Games/Dwarf Fortress/PeridexisErrant's Starter Pack 0.47.05-r01/Dwarf Fortress 0.47.05"
+SAVE="data/save/modtest"
+cp reaction_flrl_advcrafts.txt "$DF/raw/objects/"
+cp reaction_flrl_advcrafts.txt "$DF/$SAVE/raw/objects/"
